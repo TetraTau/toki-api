f05dbc4138481d7f59a8ac921f83e7cc0adbbdee
1798643961 <1798643961@qq.com>
2023-05-15 15:43:45 +0800

    feat: implements fabric lookup api

--- src/main/java/net/tetratau/toki/TokiMod.java
+++ src/main/java/net/tetratau/toki/TokiMod.java
@@ -6,6 +6,6 @@ public class TokiMod implements ModInitializer {
 
     @Override
     public void onInitialize() {
-
+        //ServerLifecycleEvents.SERVER_STARTED.register(EntityApiLookupImpl::checkSelfImplementingTypes); Toki - TODO
     }
 }
